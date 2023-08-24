d004fd873bcd24d64472f6f37406b96826bb8391
SuhasManju <87925291+SuhasManju@users.noreply.github.com>
2023-08-24 16:18:19 +0530

    Changes in database module

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from model import Base
+import os
 from sqlalchemy.orm import sessionmaker
+import dotenv
+dotenv.load_dotenv()
 from sqlalchemy import create_engine
-engine=create_engine("mysql://root:Suhas123@localhost/payroll",echo=True)
+engine=create_engine(f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_SERVER')}/payroll",echo=True)
 Base.metadata.create_all(bind=engine)
 session=sessionmaker(bind=engine)
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+import dotenv
+dotenv.load_dotenv()
+print(os.getenv("DB_USER"))
